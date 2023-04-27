31bca4355808a1db8b1afd69628361eac27c44b3
naraghababyan666 <naraghababyan666@gmail.com>
2023-04-27 05:19:01 +0400

    a

--- app/Http/Controllers/RegionController.php
+++ app/Http/Controllers/RegionController.php
@@ -9,12 +9,10 @@ use Illuminate\Support\Facades\DB;
 class RegionController extends Controller
 {
     public function filterCity($text){
-        if (strlen($text) < 2){
-            return response()->json(['success' => false, 'message' => 'Minimum string length is 2']);
+        if (strlen($text) < 4){
+            return response()->json(['success' => false, 'message' => 'Minimum string length is 2'], 403);
         }
         $result = RussiaRegions::query()->where('CityName', 'like', '%' . $text . '%')->orderBy('CitySize', 'DESC')->get();
-//        $sql = "SELECT * FROM `russia_regions` WHERE `CityName` LIKE '%Москва%'";
-//        $result = DB::select($sql);
         return response()->json(['success' => true, 'cities' => $result]);
     }
 }
