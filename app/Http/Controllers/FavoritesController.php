4427a6b98f2536682249840f15def39516620837
naraghababyan666 <naraghababyan666@gmail.com>
2023-05-12 03:50:59 +0400

    fix

--- app/Http/Controllers/FavoritesController.php
+++ app/Http/Controllers/FavoritesController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Company;
 use App\Models\Favorites;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 
 class FavoritesController extends Controller
@@ -16,13 +18,33 @@ class FavoritesController extends Controller
     }
 
     public function getFavoritesList(){
-        $favorites = Favorites::query()->where('company_id', Auth::id())->get();
+        $favorites = Favorites::query()->where('company_id', Auth::id());
+        if (Auth::user()['role_id'] == Company::IS_OWNER){
+            $favorites->where('order_type', '=', 'ride')->with('rides')->get();
+        }else if(Auth::user()['role_id'] == Company::IS_DRIVER){
+            $favorites->where('order_type', '=', 'good')->with('goods')->get();
+        }else{
+            $favorites->with(['goods', 'rides'])->get();
+        }
+        $favorites = $favorites->get();
+        foreach ($favorites as $item){
+            if(!is_null($item->rides)){
+                $item->rides->upload_city_name = ((new \App\Models\RussiaRegions)->getCityNameById($item->rides->upload_loc_id));
+                $item->rides->onload_city_name = ((new \App\Models\RussiaRegions)->getCityNameById($item->rides->onload_loc_id));
+            }else if(!is_null($item->goods)){
+                $item->goods->upload_city_name = ((new \App\Models\RussiaRegions)->getCityNameById($item->goods->upload_loc_id));
+                $item->goods->onload_city_name = ((new \App\Models\RussiaRegions)->getCityNameById($item->goods->onload_loc_id));
+            }
+
+        }
+
         return response()->json(['success' => true, 'list' => $favorites]);
+
     }
 
     public function addToFavorite(Request $request){
         $validator = Validator::make($request->all(), [
-            'order_id' => 'required|exists:goods_orders,id',
+            'order_id' => 'required',
             'order_type' => 'required'
         ]);
         if ($validator->fails()) {
--- app/Models/Favorites.php
+++ app/Models/Favorites.php
@@ -4,10 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class Favorites extends Model
 {
     use HasFactory;
 
     protected $fillable = ['company_id', 'order_id', 'order_type'];
+
+    public function relation(){
+        if(Auth::user()['role_id'] == Company::IS_OWNER){
+            return $this->belongsTo(RideOrders::class, 'order_id', 'id');
+        }else if (Auth::user()['role_id'] == Company::IS_DRIVER){
+            return $this->belongsTo(GoodsOrders::class, 'order_id', 'id');
+        }else{
+            $orders = $this->belongsTo(GoodsOrders::class, 'order_id', 'id');
+            $rides = $this->belongsTo(RideOrders::class, 'order_id', 'id');
+            return ['orders' => $orders, 'rides' => $rides] ;
+        }
+    }
+
+    public function goods(){
+        return $this->belongsTo(GoodsOrders::class, 'order_id', 'id');
+    }
+    public function rides(){
+        return $this->belongsTo(RideOrders::class, 'order_id', 'id');
+    }
 }
