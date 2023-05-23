6053ebd9897f2299dda7341075c8c4ffdb95c822
naraghababyan666 <naraghababyan666@gmail.com>
2023-05-24 03:49:42 +0400

    a

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -270,17 +270,15 @@ class CompanyController extends Controller
                             upload.CityName AS upload_city_name, onload.CityName AS onload_city_name from `ride_orders` as g
                      JOIN russia_regions upload ON g.upload_loc_id = upload.CityId
                      JOIN russia_regions onload ON g.onload_loc_id = onload.CityId
-                    where ${where_text}";
-            dd($sql);
+                    where ${where_text} ;" ;
         }else{
             $sql =  "SELECT g.id, g.company_id, g.upload_loc_id, g.onload_loc_id, g.order_title, g.kuzov_type, g.loading_type, g.start_date, g.end_date,
                             g.max_weight, g.max_volume, g.payment_type, g.payment_nds, g.ruble_per_kg, IF(${data['is_subscribed']} = 1, g.phone_number, NULL) AS phone_number,
                             g.company_name, g.is_disabled, g.created_at, IF(${data['is_subscribed']} = 1, g.description, NULL) AS order_description, g.prepaid, g.manager_id,
                             g.material_type, g.material_info, upload.CityName AS upload_city_name, onload.CityName AS onload_city_name from `ride_orders` as g
                      JOIN russia_regions upload ON g.upload_loc_id = upload.CityId
-                     JOIN russia_regions onload ON g.onload_loc_id = onload.CityId";
+                     JOIN russia_regions onload ON g.onload_loc_id = onload.CityId ;";
         }
-        dd($sql);
         $aa = DB::select($sql);
 
         return response()->json(['success' => true, 'orders' => $aa]);
