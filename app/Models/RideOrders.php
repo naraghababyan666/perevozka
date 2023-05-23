a24502333b890d988b5f84366ba0a43f8ff200d5
naraghababyan666 <naraghababyan666@gmail.com>
2023-05-24 01:16:07 +0400

    a

--- app/Models/RideOrders.php
+++ app/Models/RideOrders.php
@@ -10,6 +10,7 @@ class RideOrders extends Model
     use HasFactory;
 
 
-    protected $fillable = ['company_id', 'upload_loc_id', 'onload_loc_id', 'kuzov_type', 'loading_type',
-        'max_weight', 'max_volume', 'payment_type', 'ruble_per_kg', 'phone_number', 'company_name', 'is_disabled'];
+    protected $fillable = ['company_id', 'upload_loc_id', 'onload_loc_id', 'kuzov_type', 'loading_type', 'order_title','start_date','end_date','payment_nds',
+        'max_weight', 'max_volume', 'payment_type', 'ruble_per_kg', 'phone_number', 'company_name', 'is_disabled', 'description', 'prepaid', 'manager_id',
+        'material_type', 'material_info'];
 }
