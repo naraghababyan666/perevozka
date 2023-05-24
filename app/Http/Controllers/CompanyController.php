c8e755dafb2e8e82883447b877667369466c3f08
naraghababyan666 <naraghababyan666@gmail.com>
2023-05-24 04:26:39 +0400

    a

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -128,10 +128,12 @@ class CompanyController extends Controller
         $sql = "SELECT g.id, g.company_id, g.upload_loc_id, g.upload_loc_info, g.onload_loc_id,g.onload_loc_info, g.order_title, g.kuzov_type,
                         g.loading_type, g.start_date, g.end_date, g.max_weight, g.max_volume, g.payment_type, g.payment_nds, g.prepaid, g.ruble_per_kg,
                         g.company_name, g.is_disabled, g.created_at,g.description,g.material_type,g.material_info,
-                        upload.CityName AS upload_city_name, onload.CityName AS onload_city_name
+                        upload.CityName AS upload_city_name, onload.CityName AS onload_city_name,
+                        managers.phone_number AS phone_number, managers.FullName AS manager_name
                  from `goods_orders` as g
                  JOIN russia_regions upload ON g.upload_loc_id = upload.CityId
                  JOIN russia_regions onload ON g.onload_loc_id = onload.CityId
+                 JOIN managers managers ON g.manager_id = managers.id
                 WHERE `company_id` = '${userID}';
                 ";
         $orders = DB::select($sql);
@@ -147,10 +149,12 @@ class CompanyController extends Controller
         $userID = Auth::id();
         $sql = "SELECT g.id, g.company_id, g.upload_loc_id, g.onload_loc_id, g.order_title, g.kuzov_type,
                         g.loading_type, g.start_date, g.end_date, g.max_weight, g.max_volume, g.payment_type, g.payment_nds, g.prepaid, g.ruble_per_kg,
-                        g.company_name,g.phone_number, g.is_disabled, g.created_at,g.description,g.manager_id,g.material_type,g.material_info,
+                        g.company_name, g.is_disabled, g.created_at,g.description,g.manager_id,g.material_type,g.material_info,
+                        managers.phone_number AS phone_number, managers.FullName AS manager_name,
                         upload.CityName AS upload_city_name, onload.CityName AS onload_city_name from `ride_orders` as g
                      JOIN russia_regions upload ON g.upload_loc_id = upload.CityId
-                     JOIN russia_regions onload ON g.onload_loc_id = onload.CityId";
+                     JOIN russia_regions onload ON g.onload_loc_id = onload.CityId
+                     JOIN managers managers ON g.manager_id = managers.id WHERE `company_id` = '${userID}'";
         $rides = DB::select($sql);
         return response()->json(['success' => true, 'data' => $rides]);
     }
