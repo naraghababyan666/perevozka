e81c154cee6d7ae3b5e4facb3fb7503467992b19
naraghababyan666 <naraghababyan666@gmail.com>
2023-08-30 04:25:43 +0400

    a

--- app/Service/PaymentService.php
+++ app/Service/PaymentService.php
@@ -68,7 +68,6 @@ class PaymentService
 //            $currency = $payment->getAmount()->currency;
 //            $idempotenceKey = uniqid('', true);
             $response = $client->getPaymentInfo($paymentId);
-            dd($response->getId());
             if($response->getStatus() == 'succeeded'){
                 return true;
             }else{
