4cc0d360cf1f66c12d67a4c82d25ae42094e4c4a
naraghababyan666 <naraghababyan666@gmail.com>
2024-04-10 02:54:48 +0400

    a

--- app/Jobs/GoodsArchive.php
+++ app/Jobs/GoodsArchive.php
@@ -29,34 +29,34 @@ class GoodsArchive implements ShouldQueue
      */
     public function handle(): void
     {
-//        $goods = GoodsOrders::all();
-//        foreach ($goods as $good){
-//            if($good['is_disabled'] == '0'){
-//                if($good['end_date'] < Carbon::now()){
-//                    GoodsOrders::query()->where('id', $good['id'])->update([
-//                            'is_disabled' => '1',
-//                            'in_archive_date' => Carbon::now()->addDays(15)]
-//                    );
-//                }
-//            }else{
-//                if($good['in_archive_date'] != null){
-//                    $now = Carbon::now();
-//                    $inArchiveDate = Carbon::parse($good['in_archive_date']);
-//                    $diff = $inArchiveDate->diffInDays($now);
-//                    if($diff >= 15){
-//                        GoodsOrders::query()->where('id', $good['id'])->delete();
-//                    }
-//                }else{
-//                    GoodsOrders::query()->where('id', $good['id'])->update([
-//                            'in_archive_date' => Carbon::now()->addDays(15)]
-//                    );
-//                }
-//
-//            }
-//        }
-        $now = Carbon::now();
-        $usr = Company::query()->find(1)->first();
-        $usr->phone_number = $now->valueOf();
-        $usr->save();
+        $goods = GoodsOrders::all();
+        foreach ($goods as $good){
+            if($good['is_disabled'] == '0'){
+                if($good['end_date'] < Carbon::now()){
+                    GoodsOrders::query()->where('id', $good['id'])->update([
+                            'is_disabled' => '1',
+                            'in_archive_date' => Carbon::now()->addDays(15)]
+                    );
+                }
+            }else{
+                if($good['in_archive_date'] != null){
+                    $now = Carbon::now();
+                    $inArchiveDate = Carbon::parse($good['in_archive_date']);
+                    $diff = $inArchiveDate->diffInDays($now);
+                    if($diff >= 15){
+                        GoodsOrders::query()->where('id', $good['id'])->delete();
+                    }
+                }else{
+                    GoodsOrders::query()->where('id', $good['id'])->update([
+                            'in_archive_date' => Carbon::now()->addDays(15)]
+                    );
+                }
+
+            }
+        }
+//        $now = Carbon::now();
+//        $usr = Company::query()->find(1)->first();
+//        $usr->phone_number = $now->valueOf();
+//        $usr->save();
     }
 }
